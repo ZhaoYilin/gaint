5158895f69c8f5382c3bbf015aa036c917bc55b7
ZhaoYilin <zhaoyilin1991@gmail.com>
2023-05-08 22:55:04 +0800

    documentation update

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# moha documentation build configuration file, created by
+# gaint documentation build configuration file, created by
 # sphinx-quickstart on Fri Apr 20 03:14:34 2018.
 #
 # This file is execfile()d with the current directory set to its
@@ -61,8 +61,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'moha'
-copyright = u'2022, Yilin Zhao'
+project = u'gaint'
+copyright = u'2023, Yilin Zhao'
 author = u'Yilin Zhao'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -136,7 +136,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'mohadoc'
+htmlhelp_basename = 'gaintdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -162,7 +162,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'moha.tex', u'moha Documentation',
+    (master_doc, 'gaint.tex', u'gaint Documentation',
      u'Yilin Zhao', 'manual'),
 ]
 
@@ -172,7 +172,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'moha', u'moha Documentation',
+    (master_doc, 'gaint', u'gaint Documentation',
      [author], 1)
 ]
 
@@ -183,8 +183,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'moha', u'moha Documentation',
-     author, 'moha', 'One line description of project.',
+    (master_doc, 'gaint', u'gaint Documentation',
+     author, 'gaint', 'One line description of project.',
      'Miscellaneous'),
 ]
 
--- sphinx/source/index.rst
+++ sphinx/source/index.rst
@@ -1,11 +1,11 @@
-.. moha documentation master file, created by
+.. gaint documentation master file, created by
    sphinx-quickstart on Fri Apr 20 03:14:34 2018.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-MoHa |version|
-##############
-**MoHa** is the abbreviation of Molecular Hamiltonian. It is a general-purpose electronic structure program written in python that emphasizes code simplicity and facilitates new method development. The package provides the standard module including custom Hamiltonians, using mean-field and post-mean-field methods with standard Gaussian basis functions, post-process for varieties properties.
+GaInt |version|
+###############
+**GaInt** is the abbreviation of Gaussian Integral, it is a python package for the non-normalizecd molecular integrals over Primitive Cartesian Gaussian orbitals. This is a particularly naive implementation in python: little attempt is made to conserve memory or CPU time. Nevertheless, it is useful for small test calculations, in particular for investigating ideas quantum chemistry.
 
 Contents
 ========
@@ -13,17 +13,10 @@ Contents
 .. toctree::
    :maxdepth: 2
    :numbered:
-   :caption: QUICK START:
-
-   quick_start/installation
-   quick_start/example
-
-.. toctree::
-   :maxdepth: 2
-   :numbered:
    :caption: USER DOCUMENTATION:
 
    user/introduction
+   user/installation
    user/molecule
    user/basis
    user/obara_saika
